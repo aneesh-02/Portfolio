e552dbf067d12e58c34fb2b829e1461ee88ced08
unknown <aneeshsharmachd@gmail.com>
2021-06-19 00:48:23 +0530

    JS smooth scroll : fixed

--- script.js
+++ script.js
@@ -2,32 +2,31 @@
 // The anchor tagxs take us to the link abruptly
 // We can make a scrolling effect using JS
 
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');  // getting all the 14 nav menu elements in an array
+var interval;
 
-var navMenuAnchorTags = document.querySelectorAll(".nav-menu a");
 
-var interval
+for (var i = 0; i < navMenuAnchorTags.length; i++) { // iterating over nav menu elements
+    navMenuAnchorTags[i].addEventListener('click', function (event) { // on click for each of them will call this function
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase(); // the text of each list item is now a targetSection ID
+        console.log(this.textContent);
+        var targetSection = document.getElementById(targetSectionID); // this ID is now used to make a target section
+        console.log(targetSection);
+        //    interval = setInterval(scrollVertically, 20, targetSection);
 
-for(var i = 0; i < navMenuAnchorTags.length;i++)
+        interval = setInterval(function () {   // new var created to call sroll function after every 20 ms.
+            scrollVertically(targetSection);
+        }, 20);
+    });
+}
 
-{
-    navMenuAnchorTags[i].addEventListener("click",function(event)
-        {
-            event.preventDefault();
-            var targetSectionID = this.textContent.trim().toLowerCase();
-            var targetSection = document.getElementById(targetSectionID);
-            
-            interval = setInterval(scrollFunction, 50, targetSection);
-        }
-    );
-}    
 
-scrollFunction(targetSection) 
-{
-    var targetSectionCoordinates = targetSection.getBoundingClientRect();
-    if (targetSectionCoordinates.top <= 0) 
-     {
+function scrollVertically(targetSection) { // on clicking a funtion is called which calls setinterval func which calls scroll function
+    var targetSectionCoordinates = targetSection.getBoundingClientRect(); // gets the target section's top pixel
+    if (targetSectionCoordinates.top <= 0) { // keep scrolling till top of the target section is reached
         clearInterval(interval);
         return;
     }
-    window.scrollBy(0, 50);
+    window.scrollBy(0, 50); // keep scrolling by 50 px every 20 ms
 }
