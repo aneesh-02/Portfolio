feda663c1e864fed8a438b8fa8a799244c872053
unknown <aneeshsharmachd@gmail.com>
2021-06-24 00:17:20 +0530

    Skill section autofill implemented using JS

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ for (var i = 0; i < navMenuAnchorTags.length; i++) { // iterating over nav menu 
     navMenuAnchorTags[i].addEventListener('click', function (event) { // on click for each of them will call this function
         event.preventDefault();
         var targetSectionID = this.textContent.trim().toLowerCase(); // the text of each list item is now a targetSection ID
-        console.log(this.textContent);
+       // console.log(this.textContent);
         var targetSection = document.getElementById(targetSectionID); // this ID is now used to make a target section
-        console.log(targetSection);
+        //console.log(targetSection);
         //    interval = setInterval(scrollVertically, 20, targetSection);
 
         interval = setInterval(function () {   // new var created to call sroll function after every 20 ms.
@@ -30,3 +30,57 @@ function scrollVertically(targetSection) { // on clicking a funtion is called wh
     }
     window.scrollBy(0, 50); // keep scrolling by 50 px every 20 ms
 }
+
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
+// This event fills the progress bars if they are displayed on the screen when the page is loaded.
+//window.addEventListener("load", checkScroll);
